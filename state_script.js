2aad9c4cb5391566dd0369b6403aa9d3ff4cb676
pfdev1 <151556120+pfdev1@users.noreply.github.com>
2023-11-20 11:35:24 -0800

    Update state_script.js

--- state_script.js
+++ state_script.js
@@ -3,4 +3,3 @@
          document.body.innerHTML = document.body.innerHTML.replace("America", json.region);
      }
 })();
-<script type="application/javascript" src="https://api.ipdata.co?api-key=37bd42ba38ae036c438a02ad99db73d3af47ffe552a1217cd2961a2d&callback=getIP"></script>
