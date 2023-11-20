00c9da562059b06e27e71d2d323c1d3f65a87917
pfdev1 <151556120+pfdev1@users.noreply.github.com>
2023-11-20 14:39:41 -0800

    Create elem-state.js

---
+++ elem-state.js
@@ -0,0 +1,9 @@
+function getIP(json) {
+     if (json && json.region) {
+         document.getElementById(title).innerHTML = document.getElementById(title).innerHTML.replace("America", json.region);
+     }
+}
+let script = document.createElement("script");
+script.type = "application/javascript";
+script.src = "https://api.ipdata.co?api-key=37bd42ba38ae036c438a02ad99db73d3af47ffe552a1217cd2961a2d&callback=getIP";
+document.head.appendChild(script);
