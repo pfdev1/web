00e4ef61606f63215b444c50645e6b0db2f24dc1
pfdev1 <151556120+pfdev1@users.noreply.github.com>
2023-11-20 14:20:36 -0800

    Update state_script.js

--- state_script.js
+++ state_script.js
@@ -1,9 +1,16 @@
+function getIP(json) {
+     console.log('here');
+     if (json && json.region) {
+         document.body.innerHTML = document.body.innerHTML.replace("America", json.region);
+     }
+}
 let script = document.createElement("script");
 script.type = "application/javascript";
 script.src = "https://api.ipdata.co?api-key=37bd42ba38ae036c438a02ad99db73d3af47ffe552a1217cd2961a2d&callback=getIP";
 document.head.appendChild(script);
 
-script.addEventListener('load', function getIP(json) {
+script.addEventListener('load', function (json) {
+     console.log('hi');
      console.log(json);
      if (json && json.region) {
          document.body.innerHTML = document.body.innerHTML.replace("America", json.region);
